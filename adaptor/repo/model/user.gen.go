f8d7b1423eac46cadcf209a19709ee752ae3d17d
井科伟 <jingkewei456@gmail.com>
2025-12-27 00:24:11 +0800

    fix: 优化User模型定义并统一使用model.User创建用户
      - User模型字段添加明确的varchar类型定义
      - LastLogin字段改为指针类型支持nil
      - Register改用model.User直接创建替代dto.UserModel

--- adaptor/repo/model/user.gen.go
+++ adaptor/repo/model/user.gen.go
@@ -9,12 +9,12 @@ const TableNameUser = "user"
 // User C端用户表
 type User struct {
 	ID        int64     `gorm:"column:id;primaryKey;autoIncrement:true" json:"id"`
-	Nickname  string    `gorm:"column:nickname;not null;comment:昵称" json:"nickname"`
-	Avatar    string    `gorm:"column:avatar;not null;default:'';comment:头像" json:"avatar"`
-	Mobile    string    `gorm:"column:mobile;not null;uniqueIndex;comment:手机号" json:"mobile"`
-	Password  string    `gorm:"column:password;not null;default:'';comment:密码" json:"password"`
-	OpenID    string    `gorm:"column:open_id;not null;default:'';index;comment:微信OpenID" json:"open_id"`
-	UnionID   string    `gorm:"column:union_id;not null;default:'';index;comment:微信UnionID" json:"union_id"`
+	Nickname  string    `gorm:"column:nickname;type:varchar(64);not null;default:'';comment:昵称" json:"nickname"`
+	Avatar    string    `gorm:"column:avatar;type:varchar(255);not null;default:'';comment:头像" json:"avatar"`
+	Mobile    string    `gorm:"column:mobile;type:varchar(20);not null;uniqueIndex;comment:手机号" json:"mobile"`
+	Password  string    `gorm:"column:password;type:varchar(128);not null;default:'';comment:密码" json:"password"`
+	OpenID    string    `gorm:"column:open_id;type:varchar(64);not null;default:'';index;comment:微信OpenID" json:"open_id"`
+	UnionID   string    `gorm:"column:union_id;type:varchar(64);not null;default:'';index;comment:微信UnionID" json:"union_id"`
 	Gender    int32     `gorm:"column:gender;not null;default:0;comment:性别 0未知 1男 2女" json:"gender"`
 	Status    int32     `gorm:"column:status;not null;default:1;comment:状态 1正常 -1禁用" json:"status"`
 	CreateAt  time.Time `gorm:"column:create_at;autoCreateTime" json:"create_at"`
